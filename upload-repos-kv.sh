2dc7ab7e438ef998e42d1e60b2283fe915b3026d
Lleyton Gray <lleyton@fyralabs.com>
2024-03-22 20:55:30 -0700

    feat: add repo upload script

---
+++ upload-repos-kv.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+: "${LOCAL:=false}"
+
+for filename in repos/*.json; do
+  base="$(basename $filename)"
+  key="${base%%.*}"
+  pnpm wrangler kv:key put --binding=TETSUDOU --local="$LOCAL" "$key" "$(cat $filename)"
+done
