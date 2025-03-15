868a54345cc1b5d0d579bbcb3909e4eb3db8e74f
lleyton@fyralabs.com <lleyton@fyralabs.com>
2025-03-14 17:48:47 -0700

    fix: workaround hono arktype middleware bug

--- upload-repos-kv.sh
+++ upload-repos-kv.sh
@@ -5,5 +5,5 @@
 for filename in repos/*.json; do
   base="$(basename $filename)"
   key="${base%%.*}"
-  pnpm wrangler kv:key put --binding=TETSUDOU --local="$LOCAL" "mirrors/$key" "$(cat $filename)"
+  pnpm wrangler kv key put --binding=TETSUDOU --local="$LOCAL" "mirrors/$key" "$(cat $filename)"
 done
